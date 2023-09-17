ff0869b6c45d811cb58cd8ec79435be16305a36c
Kittipong Pounglaya <kp.Pounglaya@gmail.com>
2023-09-17 14:30:39 +0700

    commit

---
+++ app.js
@@ -0,0 +1,59 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const app = express();
+const port = 3000;
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {
+  useNewUrlParser: true,
+});
+
+const User = require("./model/user");
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.post("/login", async function (req, res) {
+  try {
+    const { username, password } = req.body;
+    const isLogin = await User.findOne({ email: username });
+
+    if (isLogin.password === password) {
+      res.render("secrets");
+    } else {
+      res.redirect("/");
+    }
+  } catch (error) {}
+});
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.post("/register", async function (req, res) {
+  try {
+    //console.log(req.body);
+    const user = new User({
+      email: req.body.username,
+      password: req.body.password,
+    });
+    const savedUser = await user.save();
+    console.log("Register successfully :" + savedUser);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log("Starting server on port " + port);
+});
