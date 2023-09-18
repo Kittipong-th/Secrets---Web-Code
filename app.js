69f23905c963f53e0bcfaa1c0d4df9c302ce782e
Kittipong Pounglaya <kp.Pounglaya@gmail.com>
2023-09-18 14:12:17 +0700

    connect with google oauth level-5

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const User = require("./model/user");
 
@@ -36,12 +37,61 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB", {
 });
 
 passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, {
+      id: user.id,
+      username: user.username,
+      picture: user.picture,
+    });
+  });
+});
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
 
 app.get("/", function (req, res) {
   res.render("home");
 });
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    if (!req.user) {
+      // Authentication failed or user not found, handle accordingly
+      return res.redirect("/error");
+    }
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  }
+);
+
 app.get("/login", function (req, res) {
   res.render("login");
 });
@@ -54,11 +104,16 @@ app.post(
   })
 );
 
-app.get("/secrets", function (req, res) {
-  if (req.isAuthenticated()) {
-    res.render("secrets");
-  } else {
-    res.redirect("/login");
+app.get("/secrets", async function (req, res) {
+  try {
+    const foundUsers = await User.find({ secret: { $ne: null } }).exec();
+    if (foundUsers) {
+      res.render("secrets", { usersWithSecrets: foundUsers });
+    }
+  } catch (err) {
+    console.log(err);
+    // Handle the error appropriately, e.g., render an error page or redirect
+    res.redirect("/error");
   }
 });
 
@@ -80,6 +135,35 @@ app.post("/register", async function (req, res) {
   });
 });
 
+app.get("/submit", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("submit");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/submit", async function (req, res) {
+  const { secret } = req.body;
+  console.log(req.user.id);
+
+  try {
+    const foundUser = await User.findById(req.user.id);
+    if (foundUser) {
+      foundUser.secret = secret;
+      await foundUser.save();
+      res.redirect("/secrets");
+    } else {
+      console.log("User not found");
+      // Handle the case where the user is not found
+    }
+  } catch (err) {
+    console.log(err);
+    // Handle the error appropriately, e.g., render an error page or redirect
+    res.redirect("/error");
+  }
+});
+
 app.get("/logout", function (req, res) {
   req.logout(function (err) {
     if (err) {
@@ -88,6 +172,7 @@ app.get("/logout", function (req, res) {
     res.redirect("/");
   });
 });
+
 app.listen(port, () => {
   console.log("Starting server on port " + port);
 });
--- model/user.js
+++ model/user.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+const findOrCreate = require("mongoose-findorcreate");
 // const encrypt = require("mongoose-encryption");
 const passportLocalMongoose = require("passport-local-mongoose");
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
+  googleId: String,
+  secret: String,
 });
 
 // const secret = process.env.secret;
@@ -13,4 +16,5 @@ const userSchema = new mongoose.Schema({
 //   encryptedFields: ["password"],
 // });
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 module.exports = mongoose.model("User", userSchema);
