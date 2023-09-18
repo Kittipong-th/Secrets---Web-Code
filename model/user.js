3d8719744a38d44ee9619f669dc4733ee7d3a128
Kittipong Pounglaya <kp.Pounglaya@gmail.com>
2023-09-18 07:48:12 +0700

    update env/git

--- model/user.js
+++ model/user.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
@@ -6,7 +7,7 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = "kppounglaya";
+const secret = process.env.secret;
 userSchema.plugin(encrypt, {
   secret: secret,
   encryptedFields: ["password"],
