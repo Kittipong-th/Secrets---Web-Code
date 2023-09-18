619e468fa4d606d1f57cc5d14d7581bf9141ed5f
Kittipong Pounglaya <kp.Pounglaya@gmail.com>
2023-09-18 07:38:37 +0700

    Encrypt level-2

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+
 const app = express();
 const port = 3000;
 
--- model/user.js
+++ model/user.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
+});
+
+const secret = "kppounglaya";
+userSchema.plugin(encrypt, {
+  secret: secret,
+  encryptedFields: ["password"],
 });
 
 module.exports = mongoose.model("User", userSchema);
